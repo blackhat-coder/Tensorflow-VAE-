aae1dcc04362cda6ba32e406361dcde758d2c202
Aboh Israel <abohisrael100@gmail.com>
2021-12-20 14:48:46 +0100

    Debug model def

--- model_def.py
+++ model_def.py
@@ -12,7 +12,7 @@ class Encoder(keras.Model):
     def __init__(self, **kwargs):
         super(Encoder, self).__init__(**kwargs)
 
-        self.input_layer = keras.layers.Input(shape=(INPUT_SIZE))
+        # self.input_layer = keras.layers.Input(shape=(INPUT_SIZE))
         
         self.conv_layer_one = keras.layers.Conv2D(64, 3, padding="same", activation="relu")
         self.conv_layer_two = keras.layers.Conv2D(64, 3, padding="same", activation="relu") #shape = (28, 28, 64)
@@ -25,26 +25,26 @@ class Encoder(keras.Model):
     
     def __call__(self, inputs):
 
-        inputs = self.input_layer()(inputs)
-        inputs = self.conv_layer_one()(inputs)
-        inputs = self.conv_layer_two()(inputs)
-        inputs = self.pool_one()(inputs)
+        # inputs = self.input_layer(inputs)
+        inputs = self.conv_layer_one(inputs)
+        inputs = self.conv_layer_two(inputs)
+        inputs = self.pool_one(inputs)
 
-        inputs = self.conv_layer_three()(inputs)
-        inputs = self.conv_layer_four()(inputs)
-        inputs = self.pool_two()()(inputs)
+        inputs = self.conv_layer_three(inputs)
+        inputs = self.conv_layer_four(inputs)
+        inputs = self.pool_two(inputs)
 
         flatten = keras.layers.Flatten()(inputs)
 
         mean = keras.layers.Dense(LATENT_DIM)(flatten)
         mu_ = keras.layers.Dense(LATENT_DIM)(flatten)
 
-        enc_model = keras.models.Model( 
-            inputs=[inputs], 
-            outputs=[mean, mu_]
-            )
+        # enc_model = keras.models.Model( 
+        #     inputs=[inputs], 
+        #     outputs=[mean, mu_]
+        # )
         
-        return enc_model(inputs)
+        return (mean, mu_)
     
     
     def get_config(self):
@@ -61,10 +61,10 @@ class Decoder(keras.Model):
         self.reshape_layer = keras.layers.Reshape((7, 7, 128))
         
 
-        self.conv_transpose_one = keras.layers.Conv2DTranspose(128, kernel_size=3, padding="valid", activation="relu") #shape = (14, 14, 128)
+        self.conv_transpose_one = keras.layers.Conv2DTranspose(128, strides=2, kernel_size=3, padding="same", activation="relu") #shape = (14, 14, 128)
         self.conv_transpose_two = keras.layers.Conv2DTranspose(128, kernel_size=3, padding="same", activation="relu")
 
-        self.conv_transpose_three = keras.layers.Conv2DTranspose(64, kernel_size=3, padding="valid", activation="relu") #shape = (28, 28, 64)
+        self.conv_transpose_three = keras.layers.Conv2DTranspose(64, strides=2, kernel_size=3, padding="same", activation="relu") #shape = (28, 28, 64)
         self.conv_transpose_four = keras.layers.Conv2DTranspose(64, kernel_size=3, padding="same", activation="relu")
 
         self.conv_transpose_five = keras.layers.Conv2DTranspose(1, kernel_size=3, padding="same", activation="relu") #shape = (28, 28, 1)
@@ -112,7 +112,7 @@ class Sampling(keras.layers.Layer):
         '''
 
         mean, log_var = inputs
-        sample = tf.random.normal((LATENT_DIM, 1)) * tf.exp(log_var / 2) + mean
+        sample = tf.random.normal([LATENT_DIM]) * tf.exp(log_var / 2) + mean
 
         return sample
 
@@ -120,22 +120,32 @@ class VAE(keras.Model):
     # Implement Holding the latent Vector
 
     def __init__(self, **kwargs):
-        super(VAE, super).__init__(**kwargs)
+        super(VAE, self).__init__(**kwargs)
 
         self.encoder = Encoder()
         self.decoder = Decoder()
     
     
     def __call__(self, inputs):
+        '''
+        Args:
+            -input : inputs , shape = (batch, INPUT_SIZE , INPUT_SIZE)
+
+            -output : (reconstruction, mean, log_var)
+                reconstruction : shape = (batch, INPUT_SIZE, INPUT_SIZE)
+                mean           : shape = (LATENT_DIM, 1)
+                log_var        : shape = (LATENT_DIM, 1)
+    
+        '''
 
         inputs = inputs
 
-        mean, log_var = self.encoder()(inputs) #shape : INPUT_SIZE
+        mean, log_var = self.encoder(inputs) #shape : INPUT_SIZE
 
         latent_vector = Sampling()((mean, log_var))
 
-        reconstruction = self.decoder()(latent_vector)
+        reconstruction = self.decoder(latent_vector)
 
-        vae_model = keras.models.Model(inputs=[inputs], outputs=[reconstruction])
+        # vae_model = keras.models.Model(inputs=[inputs], outputs=[reconstruction])
 
-        return reconstruction
+        return reconstruction, mean, log_var
